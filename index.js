06fadff48af47f29e1d2849b7cf1b0dcc6055a65
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-11 14:11:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,7 +54,6 @@ function removeFromCart(item) {
 
     if(cart[i].hasOwnProperty(item)){
       cart.splice(i,1);
-      //return cart;
     }
   }
 
@@ -64,4 +63,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(!cardNumber){
+    return "Sorry, we don't have a credit card on file for you.";
+  }
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  return cart = [];
 }
