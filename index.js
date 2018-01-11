1fa8aaaadeea9a3c5488c5cc912483f29c77db1d
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-11 14:11:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(!cardNumber){
-    return "Sorry, we don't have a credit card on file for you.";
+    console.log("Sorry, we don't have a credit card on file for you.");
   }
   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
   return cart = [];
